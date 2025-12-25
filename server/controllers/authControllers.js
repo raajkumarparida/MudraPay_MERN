9fa650b5df566584ada7f8ca1b09cc7fe87c1045
Raaj Kumar Parida <139622232+raajkumarparida@users.noreply.github.com>
2025-12-25 17:41:39 +0530

    update

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -47,8 +47,8 @@ export const register = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      secure: true,
+      sameSite: 'none',
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -132,8 +132,8 @@ export const login = async (req, res)=> {
 
         res.cookie('token', token, {
             httpOnly : true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+            secure: true,
+            sameSite: 'none',
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
 
